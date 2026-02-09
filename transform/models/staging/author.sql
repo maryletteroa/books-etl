1eea997c0097785b62f342d43a0b15aa4ab3e475
Marylette Roa <maryletteroa@gmail.com>
2026-02-09 15:05:06 +0800

    update transform models

--- transform/models/staging/author.sql
+++ transform/models/staging/author.sql
@@ -1,10 +1,8 @@
-{{ config(schema='stg') }}
-
-select str_split((column4) ->> 'key','/')[-1] as id,
-    (column4) ->> 'birth_date' as birth_date,
-    (column4) ->> 'name' as name,
-    (column4) ->> 'title' as title,
-    case when json_extract(column4, '$.bio.value') is not null then (column4) ->> 'bio' ->> 'value' else (column4) ->> 'bio' end  as bio,
-    CAST((column4) ->> 'created' ->> 'value' AS TIMESTAMP) as created,
-    CAST((column4) ->> 'last_modified' ->> 'value' AS TIMESTAMP) as last_modified
+select str_split((data) ->> 'key','/')[-1] as id,
+    (data) ->> 'birth_date' as birth_date,
+    (data) ->> 'name' as name,
+    (data) ->> 'title' as title,
+    case when json_extract(data, '$.bio.value') is not null then (data) ->> 'bio' ->> 'value' else (data) ->> 'bio' end  as bio,
+    CAST((data) ->> 'created' ->> 'value' AS TIMESTAMP) as created,
+    CAST((data) ->> 'last_modified' ->> 'value' AS TIMESTAMP) as last_modified
 from {{source('books', 'author') }}
--- transform/models/staging/rating.sql
+++ transform/models/staging/rating.sql
@@ -1,5 +1,16 @@
-select str_split((column0),'/')[-1] as work_id,
+/*select str_split((data),'/')[-1] as work_id,
     str_split((column1),'/')[-1] as book_id,
     column2 as rating,
     column3 as date
+from {{source('books', 'rating') }}
+*/
+
+
+select data ->> 'summary' ->> 'average' as average,
+	data ->> 'summary' ->> 'count' as count,
+	data ->> 'counts' ->> '1' as rating_1,
+	data ->> 'counts' ->> '2' as rating_2,
+	data ->> 'counts' ->> '3' as rating_3,
+	data ->> 'counts' ->> '4' as rating_4,
+	data ->> 'counts' ->> '5' as rating_5,
 from {{source('books', 'rating') }}
